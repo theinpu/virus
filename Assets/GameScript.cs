398e5f978a4e7deec9684d2c3199a79238df437d
theinpu <theinpu@gmail.com>
2014-05-06 19:25:02 +0400

    base version

---
+++ Assets/GameScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	public GameObject Cell;
+	public int Width = 64;
+	public int Height = 64;
+	private GameObject[,] map;
+
+	// Use this for initialization
+	void Start () {
+		map = new GameObject[Width, Height];
+		var halfWidth = Width / 2f;
+		var halfHeight = Height / 2f;
+		for(int x = 0; x < Width; x++) {
+			for(int y = 0; y < Height; y++) {
+				var gameObject = (GameObject)Instantiate (Cell, new Vector3 (x - halfWidth, 0f, y - halfHeight), Quaternion.identity);
+				gameObject.renderer.materials[0].color = new Color((float)x/Width, (float)y/Height, ((float)(x + y)/(Width + Height))/1.5f);
+				map [x, y] = gameObject;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/VirusCell.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirusCell : MonoBehaviour {
+
+	#region Public fields
+
+	public float Endurance = 1;
+	public float Strength = 1;
+	public float Dexterity = 1;
+	public float ReproductiveSpeed = 1f;
+
+	public bool CanReproduce = false;
+	public bool CanChangeHealth = true;
+	public bool CanAttack = false;
+
+
+	#endregion
+
+	#region Fields
+
+	private float health;
+	private float healthChangeRate = 0;
+
+	private float age;
+	private Vector2 reproductiveAgeBounds;
+
+	private float mutationFactor;
+
+	private float reproductiveTimer = 0;
+	private float attackTimer = 0;
+	private float healthChangeTimer = 0;
+
+	#endregion
+
+	#region Properties
+
+	public float Health { get{ return health; }}
+	public float Age { get { return age; }}
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
